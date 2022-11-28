144e62985b651bb406a0181faecb5b911906bbdf
alexns <alexns9419@yandex.ru>
2022-11-29 01:12:42 +0500

    zadacha_02

---
+++ zadacha_02/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+
+int number1 = int.Parse (Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+
+int number2 = int.Parse (Console.ReadLine());
+
+if (number1 > number2)
+{
+   
+    {
+        Console.Write($"Максимальное значение {number1} , минимальное значение {number2}" );
+        
+    }
+}
+else
+{
+   
+    {
+       Console.Write($"Максимальное значение {number2} , минимальное значение {number1}" );
+    }
+}
+25
