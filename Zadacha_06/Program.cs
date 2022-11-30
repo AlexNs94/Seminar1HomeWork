6db030fbe813b8f41884c101aed679dfaf691082
alexns <alexns9419@yandex.ru>
2022-12-01 01:37:53 +0500

    Zadacha_06

---
+++ Zadacha_06/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write ("Введите четное число : ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine ("Четное!");
+}
+else
+{
+     Console.WriteLine ("Нечетное!!");
+}
