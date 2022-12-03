2598acfee76858ca55e44c1bef72cf818bf4477d
alexns <alexns9419@yandex.ru>
2022-12-04 01:34:17 +0500

    zadacha_08

---
+++ zadacha_08/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.Write("Введите N число : ");
+
+int number = int.Parse(Console.ReadLine());
+
+int count = 0;
+
+if (number > 0)
+{
+    count = (number % 2 == 0);
+    while ( count > 0);
+    Console.Write ($"count") ; 
+    count++;
+}
+else 
+{
+    Console.Write ("У отрицательных чисел , нет четных чисел ");
+}
